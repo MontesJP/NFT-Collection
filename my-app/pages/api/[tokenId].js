c98dbe843e5573e3e13c10776822ce4b4be21422
MontesJP <juan.pablo.montes99@gmail.com>
2022-04-26 10:44:13 +0200

    Fixed route

--- my-app/pages/api/[tokenId].js
+++ my-app/pages/api/[tokenId].js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
     const tokenId = req.query.tokenId;
 
     const image_url =
-        'https://github.com/MontesJP/NFT-Collection/tree/master/my-app/public/cryptodevs';
+        'https://github.com/MontesJP/NFT-Collection/tree/master/my-app/public/cryptodevs/';
 
     res.status(200).json({
         name: 'Crypto Dev #' + tokenId,
